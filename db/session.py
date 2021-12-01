8ab715b55c3c3b518bae86501cded412b3eed4c3
Davide Mezzogori <davide.mezzogori@unipr.it>
2021-12-01 12:18:13 +0100

    [#6] SoftDelete implementation with custom KwikSession and KwikQuery

--- db/session.py
+++ db/session.py
@@ -1,47 +1,76 @@
+from __future__ import annotations
+
+from typing import Type
+
 from fastapi import Request
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.orm import Session, sessionmaker, Query
 
-from kwik.core.config import settings
+from kwik.core.config import Settings
+from kwik.db import SoftDeleteMixin, Base
 
 
-def get_db_from_request(request: Request):
-    return request.state.db
+class KwikSession(Session):
+    def delete(self, instances) -> None:
+        for instance in instances:
+            if hasattr(type(instance), "soft_delete"):
+                instance.deleted = True
+
+
+class KwikQuery(Query):
+    def __init__(self, entities: tuple[Type[Base], ...], session: Session = None) -> None:
+        super().__init__(entities, session=session)
+
+        self._soft_delete_criteria = []
+
+        if not isinstance(entities, tuple):
+            entities = (entities,)
+
+        for entity in entities:
+            if _has_soft_delete(entity):
+                criterion = entity.deleted == False
+                self._soft_delete_criteria += (criterion,)
+                self._where_criteria += (criterion,)
+
+    def ignore_soft_delete(self) -> KwikQuery:
+        self._where_criteria = [
+            c for c in self._where_criteria if c not in self._soft_delete_criteria
+        ]
+        return self
+
+    def join(self, target, *args, **kwargs):
+        if _has_soft_delete(target):
+            self._where_criteria += (target.deleted == False,)
+        return super().join(target, *args, **kwargs)
 
 
 class DBContextManager:
-    def __init__(self, db_uri=settings.SQLALCHEMY_DATABASE_URI, connect_args=None):
-        engine = create_engine(db_uri, pool_pre_ping=True, connect_args=connect_args if connect_args else {})
-        db = sessionmaker(autocommit=False, autoflush=False, bind=engine)()
+    def __init__(self, settings: Settings) -> None:
+        engine = create_engine(settings.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True)
+        db = sessionmaker(
+            class_=KwikSession,
+            query_cls=KwikQuery,
+            autocommit=False,
+            autoflush=False,
+            bind=engine,
+        )()
         self.db = db
-
-        # https://stackoverflow.com/questions/44522391/implementing-a-soft-delete-system-using-sqlalchemy
-        # https://github.com/sqlalchemy/sqlalchemy/wiki/FilteredQuery
-        # @event.listens_for(db, "do_orm_execute")
-        # def _do_orm_execute(orm_execute_state):
-        #     tables = set()
-        #     for visitor in visitors.iterate(orm_execute_state.statement):
-        #         if (
-        #             visitor.__visit_name__ == "table"
-        #             and inspect.isclass(visitor.entity_namespace)
-        #             and issubclass(visitor.entity_namespace, SoftDeleteMixin)
-        #         ):
-        #             tables.add(visitor.entity_namespace)
-        #
-        #     tables = list(tables)
-        #     if tables:
-        #         orm_execute_state.statement = orm_execute_state.statement.filter(
-        #             *(table.deleted == False for table in tables)
-        #         )
-        #     logger.error(orm_execute_state.statement)
 
     def __enter__(self) -> Session:
         return self.db
 
-    def __exit__(self, exception_type, exception_value, exception_traceback):
+    def __exit__(self, exception_type, exception_value, exception_traceback) -> None:
         if exception_type is not None:
             self.db.rollback()
         else:
             self.db.commit()
 
         self.db.close()
+
+
+def _has_soft_delete(entity: Type[Base]) -> bool:
+    return issubclass(entity, SoftDeleteMixin)
+
+
+def get_db_from_request(request: Request) -> Session:
+    return request.state.db
