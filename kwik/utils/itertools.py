68423d192dcf5c4c81910923fa4fb7a829c1e962
dmezzogori <davide@mezzogori.com>
2022-05-11 17:17:16 +0200

    itertools.iter_unique: added ability to pass lambdas to access elements attributes

--- kwik/utils/itertools.py
+++ kwik/utils/itertools.py
@@ -1,14 +1,16 @@
-from typing import Generator, TypeVar
-
+from typing import Generator, TypeVar, Callable, Iterable
 
 T = TypeVar("T")
 
 
-def iter_unique(generator: Generator[T, None, None], *, attr: str) -> T:
+def iter_unique(generator: Generator[T, None, None], *, attr: str | Callable) -> Iterable[T]:
     """restiuisce elementi unici dal generatore, in funzione del valore dell'attributo indicato"""
     seen = set()
     for t in generator:
-        x = getattr(t, attr)
+        if isinstance(attr, str):
+            x = getattr(t, attr)
+        else:
+            x = attr(t)
         if x in seen:
             continue
         seen.add(x)
