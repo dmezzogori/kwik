3f19261f6901f926d9fe889960240d2b5a8f0e1b
dmezzogori <davide@mezzogori.com>
2022-06-06 17:00:27 +0200

    introduced yield_limited in utils.itertools

--- kwik/utils/itertools.py
+++ kwik/utils/itertools.py
@@ -1,6 +1,23 @@
-from typing import Generator, TypeVar, Callable, Iterable
+from typing import Generator, TypeVar, Callable, Iterable, Any
 
 T = TypeVar("T")
+
+
+def yield_limited(*, item: Any, count: int, limit: int) -> Iterable[Any]:
+    """
+    To be used to iter over a collection of items, but limit the number of items to be returned.
+    Example:
+        >>> list(yield_limited(limit=100, l=list(range(10))))
+        >>> [0, 1, 2]
+        >>> c = 0
+        >>> for i in range(10):
+        >>>     c = yield from yield_limited(item=i, count=c, limit=3)
+    """
+
+    if count < limit:
+        yield item
+        count += 1
+    return count
 
 
 def iter_unique(generator: Generator[T, None, None], *, attr: str | Callable) -> Iterable[T]:
