5e45e4878af1c14b8091ac40e4f78cd54065f308
Davide Mezzogori <dmezzogori@users.noreply.github.com>
2025-08-11 08:43:04 +0200

    refactor: simplify main entry point by removing unnecessary function and imports

--- src/kwik/__main__.py
+++ src/kwik/__main__.py
@@ -1,20 +1,13 @@
 """Run the main entry point for the kwik command-line interface."""
 
-import sys
-
-import kwik
-
-
-def main() -> None:
-    """Run the main entry point for the kwik command-line interface."""
-    if len(sys.argv) == 1:
-        from kwik.api.api import api_router  # noqa: PLC0415
-
-        k = kwik.Kwik(api_router)
-        kwik.run(k)
-    else:
-        kwik.run(sys.argv[1])
-
-
 if __name__ == "__main__":
-    main()
+    import sys
+
+    from kwik import Kwik, run
+    from kwik.api.api import api_router
+
+    if len(sys.argv) == 1:
+        k = Kwik(api_router)
+        run(k)
+    else:
+        run(sys.argv[1])
